36677aa0b0e23c7afb1f06a75c9afba859b7233d
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-09-09 15:33:31 +0300

    calculation without Eval
    manual calculation by setting operators

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
-let display =  document.getElementById('display')
+let display =  document.getElementById('display');
+
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let reset= false;
 
 function appendValue(value) {
-    console.log(display.value)
-    display.value += value;
+    if (reset){
+        display.value = '';
+        reset = false;
+    }
+    
+    if (operator === '') {
+        firstNumber += value;
+        console.log(firstNumber)
+        display.value = firstNumber;
+    } else {
+        secondNumber += value;
+        display.value = secondNumber;
+    }
+}
+
+function setOperator(op){
+    if(firstNumber === '') return;
+    if(secondNumber !== '') calculate();
+    operator = op;
+    reset = true;
+    
 }
 
 function clearDisplay() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
     display.value = '';
 }
 
 function calculate() {
-    try {
-        display.value = eval(display.value);
+    
+        // display.value = eval(display.value);
         // eval is built-in js function, its dangerous bcoz malicious code can be executed by others
         // later we will use other expressions
-    } catch {
-        display.value = 'Error'
+
+    let result;
+    const num1 = parseFloat(firstNumber);
+    const num2 = parseFloat(secondNumber);
+
+    if (isNaN(num1) || isNaN(num2)) return;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+        break;
+        case '-':
+            result = num1 - num2;
+        break;
+        case '*':
+            result = num1 * num2;
+        break;
+        case '/':
+            result = num1 / num2;
+        break;
+        default:
+            return;
     }
+    display.value = result;
+    firstNumber = result.toString();
+    secondNumber = '';
+    operator = '';
 }
